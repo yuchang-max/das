da1e1155ac569158b92eb0a275e7ecab56d125cb
yuchang123 <15652053964@163.com>
2019-11-17 01:13:18 +0800

    第二个项目

---
+++ Sb29demoApplication.java
@@ -0,0 +1,17 @@
+package com.xdl.sb29demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb29demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb29demoApplication.class, args);
+        System.out.printf("hehe");
+
+
+    }
+
+
+}
